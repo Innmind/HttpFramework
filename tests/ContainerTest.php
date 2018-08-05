e9e4802ed040929db918957d89c2591d9b7dc80e
Baptiste Langlade <baptouuuu@gmail.com>
2018-08-05 15:23:47 +0200

    add container definition

---
+++ tests/ContainerTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\HttpFramework;
+
+use Innmind\Router\{
+    RequestMatcher,
+    Route,
+    Route\Name,
+};
+use Innmind\Http\Message\ServerRequest;
+use Innmind\Compose\ContainerBuilder\ContainerBuilder;
+use Innmind\Url\{
+    PathInterface,
+    Path,
+};
+use Innmind\Immutable\{
+    Map,
+    Set,
+    Str,
+};
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase
+{
+    public function testLoad()
+    {
+        $container = (new ContainerBuilder)(
+            new Path('container.yml'),
+            (new Map('string', 'mixed'))
+                ->put('routes', Set::of(PathInterface::class))
+                ->put('routeHandlers', new Map('string', 'callable'))
+        );
+
+        $request = $this->createMock(ServerRequest::class);
+
+        $this->assertSame(
+            404,
+            $container->get('router')($request)->statusCode()->value()
+        );
+    }
+
+    public function testOverrideRequestMatcher()
+    {
+        $container = (new ContainerBuilder)(
+            new Path('container.yml'),
+            (new Map('string', 'mixed'))
+                ->put('routes', Set::of(PathInterface::class))
+                ->put('routeHandlers', new Map('string', 'callable'))
+                ->put('requestMatcher', new class implements RequestMatcher {
+                    public function __invoke(ServerRequest $request): Route {
+                        return Route::of(new Name('foo'), Str::of('GET /'));
+                    }
+                })
+        );
+
+        $request = $this->createMock(ServerRequest::class);
+
+        $this->assertSame(
+            501,
+            $container->get('router')($request)->statusCode()->value()
+        );
+    }
+}
